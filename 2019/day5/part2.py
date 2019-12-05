c31810d77320f5e21830a4e2321a113ac4f136cb
fourjr <28086837+fourjr@users.noreply.github.com>
2019-12-05 16:22:25 +0800

    cleanup

--- 2019/day5/part2.py
+++ 2019/day5/part2.py
@@ -2,12 +2,6 @@ from enum import IntEnum
 
 with open('input.txt') as f:
     inp = [int(x) for x in f.read().split(',')]
-cursor = 0
-
-
-class Mode(IntEnum):
-    POSITION = 0
-    IMMEDIATE = 1
 
 
 def format_instruction(instruction, length):
@@ -18,103 +12,93 @@ def format_instruction(instruction, length):
     return [Mode(int(x)) for x in reversed(instruction)]
 
 
-def get_param(mode, value):
-    if mode == Mode.POSITION:
-        return inp[value]
-    elif mode == Mode.IMMEDIATE:
-        return value
+class Mode(IntEnum):
+    POSITION = 0
+    IMMEDIATE = 1
 
 
-def add(instruction):
-    instructions = format_instruction(instruction, 3)
-    inp[inp[cursor + 3]] = get_param(instructions[0], inp[cursor + 1]) + get_param(instructions[1], inp[cursor + 2])
-
-    return 4
-
-
-def multiply(instruction):
-    instructions = format_instruction(instruction, 3)
-    inp[inp[cursor + 3]] = get_param(instructions[0], inp[cursor + 1]) * get_param(instructions[1], inp[cursor + 2])
-
-    return 4
+class OpCode(IntEnum):
+    ADD = 1
+    MULTIPLY = 2
+    INPUT = 3
+    OUTPUT = 4
+    JUMP_IF_TRUE = 5
+    JUMP_IF_FALSE = 6
+    LESS_THAN = 7
+    EQUALS = 8
 
 
-def input_save(instruction):
-    inputval = 1
-    inp[inp[cursor + 1]] = inputval
+class IntCode:
+    def __init__(self, program, id_):
+        self.program = program
+        self.id = id_
+        self.cursor = 0
 
-    return 2
+    def get_param(self, mode, value):
+        if mode == Mode.POSITION:
+            return self.program[value]
+        elif mode == Mode.IMMEDIATE:
+            return value
+
+    def run(self):
+        while int(str(self.program[self.cursor])[-2:]) != 99:
+            opcode = OpCode(int(str(inp[self.cursor])[-2:]))
+            self.cursor += getattr(self, 'execute_' + opcode.name.lower())(str(self.program[self.cursor])[:-2])  # paramter is the opcode minus off last 2 digits
+
+    def execute_add(self, instruction):
+        instructions = format_instruction(instruction, 3)
+        self.program[inp[self.cursor + 3]] = self.get_param(instructions[0], self.program[self.cursor + 1]) + self.get_param(instructions[1], self.program[self.cursor + 2])
+
+        return 4
+
+    def execute_multiply(self, instruction):
+        instructions = format_instruction(instruction, 3)
+        self.program[inp[self.cursor + 3]] = self.get_param(instructions[0], self.program[self.cursor + 1]) * self.get_param(instructions[1], self.program[self.cursor + 2])
+
+        return 4
+
+    def execute_input(self, instruction):
+        self.program[self.program[self.cursor + 1]] = self.id
+
+        return 2
+
+    def execute_output(self, instruction):
+        instructions = format_instruction(instruction, 1)
+        print(self.cursor, self.get_param(instructions[0], self.program[self.cursor + 1]))
+
+        return 2
+
+    def execute_jump_if_true(self, instruction):
+        instructions = format_instruction(instruction, 2)
+        if self.get_param(instructions[0], inp[self.cursor + 1]) != 0:
+            cursor_change = self.get_param(instructions[1], self.program[self.cursor + 2]) - self.cursor
+        else:
+            cursor_change = 3
+        return cursor_change
+
+    def execute_jump_if_false(self, instruction):
+        instructions = format_instruction(instruction, 2)
+        if self.get_param(instructions[0], self.program[self.cursor + 1]) == 0:
+            cursor_change = self.get_param(instructions[1], self.program[self.cursor + 2]) - self.cursor
+        else:
+            cursor_change = 3
+        return cursor_change
+
+    def execute_less_than(self, instruction):
+        instructions = format_instruction(instruction, 3)
+        if self.get_param(instructions[0], self.program[self.cursor + 1]) < self.get_param(instructions[1], self.program[self.cursor + 2]):
+            self.program[self.program[self.cursor + 3]] = 1
+        else:
+            self.program[self.program[self.cursor + 3]] = 0
+        return 4
+
+    def execute_equals(self, instruction):
+        instructions = format_instruction(instruction, 3)
+        if self.get_param(instructions[0], self.program[self.cursor + 1]) == self.get_param(instructions[1], inp[self.cursor + 2]):
+            self.program[self.program[self.cursor + 3]] = 1
+        else:
+            self.program[self.program[self.cursor + 3]] = 0
+        return 4
 
 
-def output_stdout(instruction):
-    instructions = format_instruction(instruction, 1)
-    print(cursor, get_param(instructions[0], inp[cursor + 1]))
-
-    return 2
-
-
-def jump_if_true(instruction):
-    instructions = format_instruction(instruction, 2)
-    if get_param(instructions[0], inp[cursor + 1]) != 0:
-        cursor_change = get_param(instructions[1], inp[cursor + 2]) - cursor
-    else:
-        cursor_change = 3
-    return cursor_change
-
-
-def jump_if_false(instruction):
-    instructions = format_instruction(instruction, 2)
-    if get_param(instructions[0], inp[cursor + 1]) == 0:
-        cursor_change = get_param(instructions[1], inp[cursor + 2]) - cursor
-    else:
-        cursor_change = 3
-    return cursor_change
-
-
-def less_than(instruction):
-    instructions = format_instruction(instruction, 3)
-    if get_param(instructions[0], inp[cursor + 1]) < get_param(instructions[1], inp[cursor + 2]):
-        inp[inp[cursor + 3]] = 1
-    else:
-        inp[inp[cursor + 3]] = 0
-    return 4
-
-
-def equals(instruction):
-    instructions = format_instruction(instruction, 3)
-    if get_param(instructions[0], inp[cursor + 1]) == get_param(instructions[1], inp[cursor + 2]):
-        inp[inp[cursor + 3]] = 1
-    else:
-        inp[inp[cursor + 3]] = 0
-    return 4
-
-
-while int(str(inp[cursor])[-2:]) != 99:
-    no_of_params = 0
-    opcode = int(str(inp[cursor])[-2:])
-    if opcode == 1:
-        # Add
-        no_of_params = add(str(inp[cursor])[:-2])
-    elif opcode == 2:
-        # Multiply
-        no_of_params = multiply(str(inp[cursor])[:-2])
-    elif opcode == 3:
-        # Input
-        no_of_params = input_save(str(inp[cursor])[:-2])
-    elif opcode == 4:
-        # Output
-        no_of_params = output_stdout(str(inp[cursor])[:-2])
-    elif opcode == 5:
-        # Jump if true
-        no_of_params = jump_if_true(str(inp[cursor])[:-2])
-    elif opcode == 6:
-        # Jump if false
-        no_of_params = jump_if_false(str(inp[cursor])[:-2])
-    elif opcode == 7:
-        # Less than
-        no_of_params = less_than(str(inp[cursor])[:-2])
-    elif opcode == 8:
-        # Equals
-        no_of_params = equals(str(inp[cursor])[:-2])
-
-    cursor += no_of_params
+IntCode(inp, 5).run()
